ceb860cbca69f96c1310113a260c0e793825b1e9
cal <cal@pfb17.cshl.edu>
2018-10-16 11:49:06 -0400

    myFirstPythonScript.py

---
+++ problemsets/myFirstPythonScript.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#print my name
+import sys
+name = sys.argv[1] # get first command line parameter
+color = sys.argv[2]
+animal = sys.argv[3]
+
+
+# print
+print(name,'is my name,' , color, 'is my favorite color', 'also, my favorite animal is', animal )
+
+
