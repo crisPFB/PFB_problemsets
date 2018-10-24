c676603672e515b250e1933432342fccbdfcdf4d
cal <cal@pfb17.cshl.edu>
2018-10-23 20:26:10 -0400

    PS_biopy.py

--- problemsets/PS_biopy.py
+++ problemsets/PS_biopy.py
@@ -9,7 +9,12 @@ seqobj = Bio.Seq.Seq('atgcgaggact')
 seq_str = str(seqobj) #convert to string with str(seqobj)
 print('{} has {} mucleotides'.format( seq_str , len(seq_str)))
 
+#parsing
 for seq_record in SeqIO.parse ('Python_08.fasta', 'fasta'):
     print('ID', seq_record.id)
     print('Sequence', str(seq_record.seq))
     print ('Length', len(seq_record))
+
+#number of sequences
+from seq_record in SeqIO.parse('Python_08.fasta', 'fasta'):
+ import 
